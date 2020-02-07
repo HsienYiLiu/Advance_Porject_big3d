812b6f52bafc817bd79dec9a67cc464b6fab26bd
shaneliu5287 <shaneliu5287@gmail.com>
2020-02-07 17:34:27 -0500

    Using vector

--- readFile.cpp
+++ readFile.cpp
@@ -4,13 +4,20 @@
 #include<iostream>
 
 using namespace std;
-
+struct polyhedron{
+    int vertex_num;
+    int facet_num;
+    vector<vector<float> > vertices; 
+    vector<vector<int> > facets;
+};
 
 int main()
 {
-  float data[500][3];
+  vector<float> data[3];
+  vector<int> dataInt[3];
+  struct polyhedron instPoly;
   std::ifstream infile("0.off");
-
+  bool isInt=false;;
   string line;
   int i=0;
 
@@ -18,20 +25,35 @@ int main()
   {
     //istringstream iss(line);
     float a, b,c;
+    int ia,ib,ic;
     if (!(infile >> a >> b >> c)) { 
         break; 
     } // error
-	data[i][0]= a;
-	data[i][1]= b;
-	data[i][2]= c;
-	i++;
+	
+	if(a % 1 == 0)
+	{
+	 isInt = true;
+	 dataInt[0]= ia;
+	 dataInt[1]= ib;
+	 dataInt[2]= ic;
+	instPoly.facets.push_back(data);
+	}
+       else
+       {
+	data[0]= a;
+	data[1]= b;
+	data[2]= c;
+	instPoly.vertices.push_back(data);
+       }
+	
+	
 	//cout << a << b << c << endl;
     // process pair (a,b)
   }
 
   for(i=0;i<10;i++)
   {
-   cout << data[i][0] << " " << data[i][1] << " " <<  data[i][2] << endl;
+	cout<< instPoly.facets[i][0]] << endl;
   }
 }
 
